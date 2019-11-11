49f89a0c8eb186e3e77f80a20f131cd5c83ad5b1
Administrator <Administrator@1jung_405_03>
2019-11-11 16:15:22 +0900

    2st commit!

---
+++ OSP/src/main/java/kr/ac/tu/osp/comm/controller/CommonController.java
@@ -0,0 +1,15 @@
+package kr.ac.tu.osp.comm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "Open Source Programing..";
+		
+	}
+}
